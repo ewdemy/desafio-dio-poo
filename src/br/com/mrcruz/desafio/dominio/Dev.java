ee4243aa8dd3e3f9ab940895d88d6b494389039c
ewdemy <ewdemy@hotmail.com>
2022-03-14 23:12:45 -0300

    Aula concluindo classes de dominio

--- src/br/com/mrcruz/desafio/dominio/Dev.java
+++ src/br/com/mrcruz/desafio/dominio/Dev.java
@@ -2,6 +2,7 @@ package br.com.mrcruz.desafio.dominio;
 
 import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
@@ -10,9 +11,25 @@ public class Dev {
 	private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
 	private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
 	
-	public void inscreverBootcamp(Bootcamp bootcamp) {}
-	public void progredir() {}
-	public void calcularTotalXP() {}
+	public void inscreverBootcamp(Bootcamp bootcamp) {
+		this.conteudosInscritos.addAll(bootcamp.getConteudos());
+        bootcamp.getDevsInscritos().add(this);
+	}
+	public void progredir() {
+		Optional<Conteudo> conteudo = conteudosInscritos.stream().findFirst();
+		
+		if(conteudo.isPresent()) {
+			conteudosConcluidos.add(conteudo.get());
+			conteudosInscritos.remove(conteudo.get());
+		} else {
+			System.err.println("Você não está matriculado em nenhum curso");
+		}
+		
+	}
+	public double calcularTotalXP() {
+		return conteudosConcluidos.stream().mapToDouble(conteudo -> conteudo.calcularXP()).sum();
+	}
+	
 	public String getNome() {
 		return nome;
 	}
