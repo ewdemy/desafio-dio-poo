b1169264fbe33af4d3cb75b74bb14f676b23686c
ewdemy <ewdemy@hotmail.com>
2022-03-14 23:23:41 -0300

    Aula interligando as classes de dominio

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import java.time.LocalDate;
 
+import br.com.mrcruz.desafio.dominio.Bootcamp;
 import br.com.mrcruz.desafio.dominio.Curso;
+import br.com.mrcruz.desafio.dominio.Dev;
 import br.com.mrcruz.desafio.dominio.Mentoria;
 
 public class Main {
@@ -22,9 +24,27 @@ public class Main {
 		mentoria.setDescricao("Mentoria do curso de Java");
 		mentoria.setData(LocalDate.now());
 		
-		System.out.println(curso);
-		System.out.println(curso2);
-		System.out.println(mentoria);
+//		System.out.println(curso);
+//		System.out.println(curso2);
+//		System.out.println(mentoria);
+		
+		Bootcamp bootcamp = new Bootcamp();
+		bootcamp.setNome("Bootcamp Dev Java Samuray");
+		bootcamp.setDescricao("Descrição bootcamp java");
+		bootcamp.getConteudos().add(mentoria);
+		bootcamp.getConteudos().add(curso);
+		bootcamp.getConteudos().add(curso2);
+		
+		Dev dev = new Dev();
+		dev.setNome("Ewdemy");
+		
+		dev.inscreverBootcamp(bootcamp);
+		
+		dev.progredir();
+		
+		System.out.println("Conteúdos inscritos: " + dev.getConteudosInscritos());
+		System.out.println("Conteúdos concluidos: " + dev.getConteudosConcluidos());
+		System.out.println("XP: " + dev.calcularTotalXP());
 
 	}
 
